837c7897a8f742803302a45a4472fa5726a8350f
JonasFuchss <jonas@fuchss.de>
2025-05-07 15:13:34 +0200

    intial commit

---
+++ scraper.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import selenium
+import logging
+
+
+
+def main() -> None:
+    pass
+
+
+
+
+
+
+
+
+
+######################
+######################
+### Autoausführung ###
+######################
+######################
+
+
+if __name__ == "__main__":
+    main()
