12b528d93fe21257640b67c4cc4b5f3cb0e5b861
JonasFuchss <jonas@fuchss.de>
2025-06-25 16:07:24 +0200

    ist schon viel gscraped, aber noch nich tkomplett

--- html-parser.py
+++ html-parser.py
@@ -1,93 +1,206 @@
-
-import requests
+import asyncio
+import aiohttp
 from bs4 import BeautifulSoup
-import time
-import random
 import pandas as pd
+import re
+import random
+import logging
+import json
+from tqdm.asyncio import tqdm
+from fake_useragent import UserAgent
+import os
 
-# Beispiel-URL (anpassen!)
-BASE_URL = "https://www.autoscout24.de/angebote/mg-mg4-standard-sitz-lenkradhzg-carplay-mg-pilot-elektro-schwarz-8e835da7-2396-42f7-89bf-e14af4d3d702?sort=standard&desc=0&lastSeenGuidPresent=true&cldtidx=382&position=382&search_id=19y0owctbh3&standardSortStrategy=mia_ltr&source_otp=t50&ap_tier=t50&source=listpage_search-results&order_bucket=6&new_taxonomy_available=false&boosting_product=mia&relevance_adjustment=boost&applied_boost_level=t50&boost_level=t50"
+# === Logging ===
+LOG_FILE = "scraper.log"
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
-# User-Agent simuliert echten Browser
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                  "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+# === Config ===
+SAVE_EVERY = 50
+OUTPUT_FILE = "output.csv"
+SUCCESS_FILE = "successfully_scraped_URLs.txt"
+
+ua = UserAgent()
+sem = asyncio.Semaphore(10)
+
+BOX_IDS = [
+    "basic-details-section",
+    "listing-history-section",
+    "technical-details-section",
+    "environment-details-section",
+    "equipment-section",
+    "color-section"
+]
+
+necessary_cookies = {
+    # deine Cookies hier
 }
 
-# Liste für gesammelte Daten
-data = []
+def extract_plz(a_element):
+    if a_element and a_element.find('br'):
+        parts = list(a_element.stripped_strings)
+        plz_ort = parts[2]
+    else:
+        plz_ort = a_element.get_text(strip=True) if a_element else ""
+    match = re.match(r'(\d{5})', plz_ort)
+    return match.group(1) if match else None
 
-url = BASE_URL
-print(f"Scraping {url}")
 
-# Liste von Cookies, welche von der Website benötigt werden, damit der Cookiebanner nicht angezeigt wird.
-necessary_cookies = [
-    {
-        "name":     "_asse",
-        "value":    "cm:eyJzbSI6WyIxfDE3NDg5MzgzMjgwMjd8MHwwfDB8biIsMTgxMjAxMDMyODAyN119",
-        "secure":   False
-    },
-    {
-        "name":     "_cq_duid",
-        "value":    "1.1748938746.k917G3inM7mKC8Nh",
-        "secure":   True
-    },
-    {
-        "name":     "_cq_pxg",
-        "value":    "3|6652109758",
-        "secure":   True
-    },
-    {
-        "name":     "_cq_suid",
-        "value":    "1.1748938746.299na4GA431p7721",
-        "secure":   True
-    },
-    {
-        "name":     "addtl_consent",
-        "value":    "1~",
-        "secure":   False
-    },
-    {
-        "name":     "as24-cmp-signature",
-        "value":    "L%2BmhqL7G7PwLQWoFaJYzr9mAGTvG9Xdndl3tNFDEccicI4NjOFbBwsKevt3xnBPL%2F292Q3ORYs5VXs44zk3n%2BN4qOy4QahDz8gSkS2v%2BPWIFm7oyJqj9x6tRx9ikzjh0mIRJu8%2FIiqj6K4%2FyPtnQQO1IVBIAWOXf9a6iFQX0Mbs%3D",
-        "secure":   False
-    },
-    {
-        "name":     "as24Visitor",
-        "value":    "ca813bb2-ddcd-4568-8240-40426ec50860",
-        "secure":   False
-    },
-    {
-        "name":     "cconsent-v2",
-        "value":    "%7B%22purpose%22%3A%7B%22legitimateInterests%22%3A%5B25%5D%2C%22consents%22%3A%5B%5D%7D%2C%22vendor%22%3A%7B%22legitimateInterests%22%3A%5B10218%2C10441%2C11006%2C11005%2C11019%5D%2C%22consents%22%3A%5B%5D%7D%7D",
-        "secure":   False
-    },
-    {
-        "name":     "euconsent-v2",
-        "value":    "CQSbk4AQSbk4AGNAGCDEBjFgAAAAAAAAAAAAAAAAAADBIEQACwAKgAcAA8ACCAF4AaAA8ACYAFUAN4AfgBCQCGAIkARwAmgBhgDLAHOAO4Ae0A_AD9AI4ASUBIgChwFHgKRAWwAuQBkgDMwGrgQhAoQOgUgALAAqABwAEEALwA0AB4AEwAKYAVQAugBiADeAH6AQwBEgCOAE0AKMAYYA0QBzgDuAHtAPwA_QCLQEcAR0AkoB1AEXgJEATIAocBR4C2AFyAMkAZUAywBmYDVwHFgUIIQCgAFgBVADEAG8Ac4A7gCOAEpAOoAuQlATAAWABwAHgATAAqgBigEMARIAjgBRgD8AI4AdQBF4CRAFHgLYAZIAywCEJSA4AAsACoAHAAQQAyADQAHgATAAqgBiAD9AIYAiQBHACjAGiAOeAfgB-gEWgI4AjoBJQDqAIvASIAocBbAC5AGSAMsAhCWgCADuAI4AocBmYAAA.cAAAAAAAA4CA",
-        "secure":   False
-    },
-]
-# In ein Dictionary umwandeln, da requests keine Liste akzeptiert:
-cookies_dict = {cookie["name"]: cookie["value"] for cookie in necessary_cookies}
+def get_data_from_table(soup, table_class, table_column_class, table_row_class, existing_data):
+    def extract_from_dl(data_dl):
+        dt_elements = data_dl.find_all("dt", class_=table_column_class)
+        dd_elements = data_dl.find_all("dd", class_=table_row_class)
 
-# Request senden
-response = requests.get(url, headers=HEADERS, cookies=cookies_dict)
-if response.status_code != 200:
-    print(f"Fehler beim Laden: {response.status_code}")
+        for dt, dd in zip(dt_elements, dd_elements):
+            key = dt.get_text(strip=True)
+            ul = dd.find("ul")
+            if ul:
+                for li in ul.find_all("li"):
+                    li_text = li.get_text(strip=True)
+                    existing_data[li_text] = True
+            else:
+                if dd.find_all("p"):
+                    continue
+                else:
+                    value = dd.get_text(strip=True)
+                    existing_data[key] = value
 
-# HTML parsen
-soup: BeautifulSoup = BeautifulSoup(response.text, "html.parser")
-price_span = soup.find("span", class_="PriceInfo_price__XU0aF")
+    all_data_dl = soup.find_all("dl", class_=table_class)
+    for data_dl in all_data_dl:
+        extract_from_dl(data_dl)
 
-with open("tmp.txt", "w", encoding="utf-8") as f:
-    f.write(txt)
-    f.close()
+    hidden_containers = soup.find_all(attrs={"aria-hidden": "true"})
+    for container in hidden_containers:
+        hidden_dls = container.find_all("dl", class_=table_class)
+        for hidden_dl in hidden_dls:
+            extract_from_dl(hidden_dl)
 
-# Zufällige Pause (Bot-Erkennung vermeiden)
-time.sleep(random.uniform(1.5, 4.0))
 
-# In DataFrame umwandeln & speichern
-df = pd.DataFrame(data)
-df.to_csv("scraped_data.csv", index=False)
-print("Scraping abgeschlossen.")
+async def fetch_html(session, url):
+    headers = {"User-Agent": ua.random}
+    async with sem:
+        try:
+            async with session.get(url, headers=headers, cookies=necessary_cookies) as response:
+                await asyncio.sleep(random.uniform(1.0, 3.0))
+                if response.status != 200:
+                    logging.warning(f"[{url}] Fehler {response.status}")
+                    return None
+                return await response.text()
+        except Exception as e:
+            logging.error(f"[{url}] Verbindungsfehler: {e}")
+            return None
+
+
+async def scrape_url(session, url):
+    logging.info(f"[{url}] Scraping gestartet")
+    html = await fetch_html(session, url)
+    if not html:
+        return None
+
+    soup = BeautifulSoup(html, "html.parser")
+    result_dict = {}
+
+    # result_dict["URL"] = url
+
+    title_field = soup.find("span", class_="StageTitle_boldClassifiedInfo__sQb0l StageTitle_textOverflow__KN9BA")
+    if title_field:
+        result_dict["Titel"] = title_field.get_text(strip=True)
+
+    price_field = soup.find("span", class_="PriceInfo_price__XU0aF")
+    if price_field:
+        try:
+            result_dict["Preis"] = int(price_field.get_text(strip=True).split(sep="€ ")[1].replace(".", ""))
+        except:
+            result_dict["Preis"] = None
+
+    seller_field = soup.find("span", class_="scr-tag scr-tag--default")
+    if seller_field:
+        result_dict["Verkaufsart"] = seller_field.get_text(strip=True)
+
+    seller_location_field = soup.find("a", class_="scr-link Department_link__xMUEe")
+    plz = extract_plz(seller_location_field)
+    if plz:
+        result_dict["Verkaufsort"] = plz
+
+    for id in BOX_IDS:
+        box = soup.find(id=id)
+        if box:
+            get_data_from_table(
+                box,
+                "DataGrid_defaultDlStyle__xlLi_",
+                "DataGrid_defaultDtStyle__soJ6R",
+                "DataGrid_defaultDdStyle__3IYpG",
+                result_dict
+            )
+
+    logging.info(f"[{url}] {len(result_dict)} Datenpunkte extrahiert")
+    return result_dict
+
+
+async def scrape_all(urls):
+    scraped_data = []
+    completed = load_completed_urls()
+
+    urls_to_scrape = [url for url in urls if url not in completed]
+    total = len(urls_to_scrape)
+
+    if total == 0:
+        logging.info("Keine neuen URLs zu scrapen.")
+        return
+
+    logging.info(f"{total} URLs werden gescraped.")
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for i, url in enumerate(urls_to_scrape):
+            tasks.append(scrape_url(session, url))
+
+            if len(tasks) >= SAVE_EVERY or i == total - 1:
+                batch_results = await asyncio.gather(*tasks)
+                batch_results = [r for r in batch_results if r]
+
+                scraped_data.extend(batch_results)
+                save_results(scraped_data, OUTPUT_FILE)
+                scraped_data.clear()
+
+                write_success_urls([urls_to_scrape[j] for j in range(i + 1 - len(tasks), i + 1)])
+                logging.info(f"Zwischenspeicherung nach {i + 1} URLs abgeschlossen.")
+                tasks.clear()
+
+
+def save_results(data, filename):
+    df_new = pd.DataFrame(data)
+    if os.path.exists(filename):
+        df_existing = pd.read_csv(filename, low_memory=False)
+        df_combined = pd.concat([df_existing, df_new], ignore_index=True)
+    else:
+        df_combined = df_new
+
+    df_combined.to_csv(filename, index=False, encoding="utf-8")
+    logging.info(f"{len(df_new)} neue Einträge gespeichert ({len(df_combined)} insgesamt).")
+
+
+def write_success_urls(urls):
+    with open(SUCCESS_FILE, "a", encoding="utf-8") as f:
+        for url in urls:
+            f.write(url + "\n")
+
+
+def load_completed_urls():
+    if os.path.exists(SUCCESS_FILE):
+        with open(SUCCESS_FILE, "r", encoding="utf-8") as f:
+            return set(line.strip() for line in f if line.strip())
+    return set()
+
+
+def main():
+    with open("car_urls.txt", "r", encoding="utf-8") as f:
+        urls: list = json.load(f)
+
+    asyncio.run(scrape_all(urls))
+    logging.info("Scraping abgeschlossen.")
+
+if __name__ == "__main__":
+    main()
